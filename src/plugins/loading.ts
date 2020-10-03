97bc93c0c0c6c5fe79bf32fdaec7cbd75ca288c3
Talv <krz.krawczuk@gmail.com>
2020-10-04 01:55:35 +0200

    upgrade deps

--- src/plugins/loading.ts
+++ src/plugins/loading.ts
@@ -2,34 +2,7 @@ import Vue from 'vue';
 import Loading from 'vue-loading-overlay';
 import 'vue-loading-overlay/dist/vue-loading.css';
 
-declare class LoadingComponent extends Vue {
-    hide(): void;
-}
-
-interface LoadingServiceOptions {
-    container?: HTMLElement;
-    canCancel?: boolean;
-    onCancel?: () => any;
-    color?: string;
-    loader?: 'spinner' | 'dots' | 'bars';
-    width?: number;
-    height?: number;
-    backgroundColor?: string;
-    opacity?: number;
-    zIndex?: number;
-}
-
-interface LoadingService {
-    show(options?: LoadingServiceOptions): LoadingComponent
-}
-
-declare module 'vue/types/vue' {
-    interface Vue {
-        $loading: LoadingService;
-    }
-}
-
-Vue.use(Loading, <LoadingServiceOptions>{
+Vue.use(Loading, {
     opacity: 0.75,
     backgroundColor: '#000',
     color: '#2196f3',
--- src/shims-loading.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'vue-loading-overlay';
